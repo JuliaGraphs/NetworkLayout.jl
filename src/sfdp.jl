7a8d3d64e5e14355f5c147c793e2e9ca6dcea85b
Abhijith Anilkumar <abhijithabhayam@gmail.com>
2016-07-04 16:03:15 +0530

    Adding docs/comments

--- src/sfdp.jl
+++ src/sfdp.jl
@@ -1,13 +1,29 @@
 export layout_fdp
 
-const C = 1
-const K = 1
-dist(xj,xi,yj,yi) = sqrt((xj-xi)^2 + (yj-yi)^2)
-mag(a,b) = a^2 + b^2
-f_attr(xi,xj,yi,yj) = (dist(xi,xj,yi,yj)^2) / K
-f_repln(xi,xj,yi,yj) = -C*(K^2) / dist(xi,xj,yi,yj)
+"""
+Using the Spring-Electric model suggested by Yifan Hu
+(http://yifanhu.net/PUB/graph_draw_small.pdf)
+Forces are calculated as :
+        f_attr(i,j) = ||xi - xj||^2 / K ,     i<->j
+        f_repln(i,j) = -CK^2 / ||xi - xj|| ,  i!=j
+Arguments :
+  g      Sparse/Full Adjacency matrix of the graph
+  tol    Tolerance distance - Minimum distance between 2 nodes
+  C, K   Constants that help scale the layout
+Output :
+  x, y   Co-ordinates for the nodes
+"""
 
-function layout_fdp(g, tol)
+# Function for calculating dist b/w vectors
+dist(xj,xi,yj,yi) = sqrt((xj-xi)^2 + (yj-yi)^2)
+# Function to calculate magnitude of the vector
+mag(a,b) = a^2 + b^2
+# Calculate Attractive force
+f_attr(xi,xj,yi,yj,K) = (dist(xi,xj,yi,yj)^2) / K
+# Calculate Repulsive force
+f_repln(xi,xj,yi,yj,C,K) = -C*(K^2) / dist(xi,xj,yi,yj)
+
+function layout_fdp(g; tol=1, C=0.2, K=1)
   converged = false
   step = 1
   energy = typemax(Float64)
@@ -25,11 +41,13 @@ function layout_fdp(g, tol)
       for j in 1:N
         i == j && continue
         if g[i,j] == 1
-          fx = fx + f_attr(x[i],x[j],y[i],y[j]) * ((x[j] - x[i]) / dist(x[j],x[i],y[j],y[i]))
-          fy = fy + f_attr(x[i],x[j],y[i],y[j]) * ((y[j] - y[i]) / dist(x[j],x[i],y[j],y[i]))
+          # Attractive forces for adjacent nodes
+          fx = fx + f_attr(x[i],x[j],y[i],y[j],K) * ((x[j] - x[i]) / dist(x[j],x[i],y[j],y[i]))
+          fy = fy + f_attr(x[i],x[j],y[i],y[j],K) * ((y[j] - y[i]) / dist(x[j],x[i],y[j],y[i]))
         else
-          fx = fx + f_repln(x[i],x[j],y[i],y[j]) * ((x[j] - x[i]) / dist(x[j],x[i],y[j],y[i]))
-          fy = fy + f_repln(x[i],x[j],y[i],y[j]) * ((y[j] - y[i]) / dist(x[j],x[i],y[j],y[i]))
+          # Repulsive forces
+          fx = fx + f_repln(x[i],x[j],y[i],y[j],C,K) * ((x[j] - x[i]) / dist(x[j],x[i],y[j],y[i]))
+          fy = fy + f_repln(x[i],x[j],y[i],y[j],C,K) * ((y[j] - y[i]) / dist(x[j],x[i],y[j],y[i]))
         end
       end
       x[i] = x[i] + step * (fx / sqrt(mag(fx,fy)))
@@ -43,6 +61,7 @@ function layout_fdp(g, tol)
 end
 
 function update_step(step, energy, energy0, progress=0)
+  # cooldown step
   const t = 0.9
   if energy < energy0
     progress = progress + 1
@@ -51,13 +70,13 @@ function update_step(step, energy, energy0, progress=0)
       step = step/t
     end
   else
-    @show "2"
     progress = 0
     step = t * step
   end
 end
 
 function dist_tolerance(x,x0,y,y0,K)
+  # check whether the layout is optimal
   xt = x-x0
   yt = y-y0
   const N = size(x,1)
