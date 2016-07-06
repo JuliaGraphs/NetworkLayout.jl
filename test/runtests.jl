8b9ddd89927474165df87816e3af204c2ab8f9ba
Abhijith Anilkumar <abhijithabhayam@gmail.com>
2016-07-06 18:02:34 +0530

    Tests for SFDP

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,30 @@
 using NetworkLayout
-using Base.Test
+using LightGraphs
+using BaseTestNext
 
-# write your own tests here
-@test 1 == 1
+@testset "Testing NetworkLayout" begin
+
+  @testset "Testing SFDP" begin
+
+    @testset "Testing Jagmesh1 graph" begin
+      array = round(Int,open(readdlm,"jagmesh1.mtx"))
+      row = array[:,1]
+      col = array[:,2]
+      entry = [1 for i in 1:3600]
+      adj_matrix = sparse(row,col,entry)
+      x, y = layout_fdp(adj_matrix, tol=0.1, K=1)
+      @test size(x) == size(y)
+      @test typeof(x) == typeof(y) == Array{Float64,1}
+    end
+
+    @testset "Testing WheelGraph" begin
+      g = WheelGraph(10)
+      adj_matrix = adjacency_matrix(g)
+      x, y = layout_fdp(adj_matrix, tol=0.1, K=1)
+      @test size(x) == size(y)
+      @test typeof(x) == typeof(y) == Array{Float64,1}
+    end
+
+  end
+
+end
