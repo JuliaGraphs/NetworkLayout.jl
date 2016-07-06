e544ccae08c54a46cdad9cb77e1aa4d9e2f53034
Abhijith Anilkumar <abhijithabhayam@gmail.com>
2016-07-07 02:00:17 +0530

    Fix Tests, Enable Travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using NetworkLayout
 using LightGraphs
 using BaseTestNext
+using GeometryTypes
 
 @testset "Testing NetworkLayout" begin
 
@@ -12,17 +13,19 @@ using BaseTestNext
       col = array[:,2]
       entry = [1 for i in 1:3600]
       adj_matrix = sparse(row,col,entry)
-      x, y = layout_fdp(adj_matrix, tol=0.1, K=1)
-      @test size(x) == size(y)
-      @test typeof(x) == typeof(y) == Array{Float64,1}
+      positions = layout_fdp(adj_matrix, 2, tol=0.1, K=1)
+      @test typeof(positions) == Array{FixedSizeArrays.Point{2,Float64},1}
+      positions = layout_fdp(adj_matrix, 3, tol=0.1, K=1)
+      @test typeof(positions) == Array{FixedSizeArrays.Point{3,Float64},1}
     end
 
     @testset "Testing WheelGraph" begin
       g = WheelGraph(10)
       adj_matrix = adjacency_matrix(g)
-      x, y = layout_fdp(adj_matrix, tol=0.1, K=1)
-      @test size(x) == size(y)
-      @test typeof(x) == typeof(y) == Array{Float64,1}
+      positions = layout_fdp(adj_matrix, 2, tol=0.1, K=1)
+      @test typeof(positions) == Array{FixedSizeArrays.Point{2,Float64},1}
+      positions = layout_fdp(adj_matrix, 3, tol=0.1, K=1)
+      @test typeof(positions) == Array{FixedSizeArrays.Point{3,Float64},1}
     end
 
   end
