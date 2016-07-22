0876b5ca8a7d75276ca4e7bd591d1889f14193fd
Abhijith Anilkumar <abhijithabhayam@gmail.com>
2016-07-22 23:58:00 +0530

    WIP: MARS improvements

--- src/stress.jl
+++ src/stress.jl
@@ -13,3 +13,27 @@ function compute_approx(g,k)
   dw = diag()
   return (centres)
 end
+
+function update_anchors(g,centers,x)
+  y = x
+  for i in centers
+    sum = 0
+    for j in centers
+      if i == j
+        sum += x[j]
+      end
+    end
+    yi = (1/centers)*sum
+  end
+  for i in centers
+    sum1 = sum2 = 0
+    for j in centers
+      if i != j
+        sum1 += w[i,j]*(x[j]+d[i,j]*(x[i]-x[j])*inv(norm(x[i]-x[j])))
+        sum2 += w[i,j]
+      end
+    end
+    yi = sum1/sum2
+  end
+  return y
+end
