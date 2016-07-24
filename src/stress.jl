6d63b5ba52382951b0549d00a00026d879fdc988
Abhijith Anilkumar <abhijithabhayam@gmail.com>
2016-07-24 22:07:59 +0530

    MARS algorithm excluding matrix operations

--- src/stress.jl
+++ src/stress.jl
@@ -1,10 +1,8 @@
 
 
-function layout_stress()
 
-end
 
-function compute_approx(g,k)
+function compute_Lω(g,k)
   centres = kcentres(g,k)
   d = distances()
   m = inv(d)
@@ -15,7 +13,7 @@ function compute_approx(g,k)
 end
 
 function update_anchors(g,centers,x)
-  y = x
+  y = copy(x)
   for i in centers
     sum = 0
     for j in centers
@@ -23,7 +21,7 @@ function update_anchors(g,centers,x)
         sum += x[j]
       end
     end
-    yi = (1/centers)*sum
+    y[i] = (1/centers)*sum
   end
   for i in centers
     sum1 = sum2 = 0
@@ -33,7 +31,48 @@ function update_anchors(g,centers,x)
         sum2 += w[i,j]
       end
     end
-    yi = sum1/sum2
+    y[i] = sum1/sum2
   end
   return y
+end
+
+function findrhs(x)
+  N = size(x,1)
+  rhs = copy(x)
+  for i in 1:N
+    sum = 0
+    for j in 1:N
+      if i!=j
+        sum += (x[i]-x[j])/norm(x[i]-x[j])
+      end
+    end
+    rhs[i] = sum
+  end
+  return rhs
+end
+
+
+function dist_tolerance(Xnew,X,tol)
+  # check whether the layout is optimal
+  const N = size(Xnew,1)
+  for i in 1:N
+    if norm(Xnew[i]-X[i]) >= K*tol
+      return false
+    end
+  end
+  return true
+end
+
+function approximate_layout(g,k)
+  Lω,centers = compute_Lω(g,k)
+  Xnew = (2*rand(Point{dim, Float64}, size(g,1)) .- 1)
+  converged = false
+  while !converged
+    X = copy(Xnew)
+    rhs = findrhs(X)
+    Xnew = solve_Lω(Lω,rhs)
+    Xnew = update_anchors(g,centers,Xnew)
+    converged = dist_tolerance(Xnew,X,tol)
+  end
+  return Xnew
 end
