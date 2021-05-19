c8e8ca7636d38b9ba758d0176c8d21975f9d0848
Hans Würfel <git@wuerfel.io>
2021-05-19 16:47:43 +0200

    set up CI: Docs, compat helper and register

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+using NetworkLayout
+using Documenter
+
+DocMeta.setdocmeta!(NetworkLayout, :DocTestSetup, :(using NetworkLayout); recursive=true)
+
+makedocs(; modules=[NetworkLayout],
+         repo="https://github.com/JuliaGraphs/NetworkLayout.jl/blob/{commit}{path}#{line}",
+         sitename="NetworkLayout.jl",
+         format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
+                                canonical="https://juliagraphs.org/NetworkLayout.jl", assets=String[]),
+         pages=["Home" => "index.md",])
+
+# if gh_pages branch gets to big, check out
+# https://juliadocs.github.io/Documenter.jl/stable/man/hosting/#gh-pages-Branch
+deploydocs(;repo="github.com/JuliaGraphs/NetworkLayout.jl",
+           push_preview=true)
